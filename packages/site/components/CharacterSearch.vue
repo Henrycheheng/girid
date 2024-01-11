a760fd13128cfc5ddea9104a68cf76716e11f461
koi <1184563958@qq.com>
2024-01-11 23:37:19 +0800

    test: codetime

--- packages/site/components/CharacterSearch.vue
+++ packages/site/components/CharacterSearch.vue
@@ -4,10 +4,15 @@
 onMounted(async () => {
   // console.log(await bangumiApi.characters({ character_id: 3 }).get())
 })
+
+const app = () => {
+  console.log('1 :>> ', 1);
+}
 </script>
 
 <template>
   <div>
     <!--  -->
+
   </div>
 </template>
